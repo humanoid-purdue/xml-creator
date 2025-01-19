38bc1f7c0b0303c6b8211e786cd390bd3661199f
henrytsay <tsay.henry@gmail.com>
2025-01-19 11:50:16 -0500

    input com data into csv

--- com_adjuster.py
+++ com_adjuster.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 
-def adjust(mass, com, com_offset, quat, motors):
+def adjust(mass, com, com_offset, quat, extra_masses):
     translated_com = com - com_offset
 
     r  = R.from_quat(quat)
@@ -9,22 +9,48 @@ def adjust(mass, com, com_offset, quat, motors):
 
     adjusted_com = rotated_com * mass
     total_mass = mass
-    for motor in motors:
-        total_mass += 0.53
-        adjusted_com += motor * 0.53
+    for extra_mass in extra_masses:
+        total_mass += extra_mass[0]
+        adjusted_com += extra_mass[1:] * extra_mass[0]
     
     adjusted_com /= total_mass
     
     return adjusted_com
 
-mass = 1.165732
-com = np.array([0.0762, 0.021336, 0])
-com_offset = np.array([0.0762, -0.01905, 0])
+def calculate_com(multiplier):
+    with open("com.csv", "r") as f:
+        data = [line.split(",") for line in f]
 
-quat = np.array([0.5, 0.5, 0.5, 0.5])
-motors = np.array([[0, -0.05715, 0], [0, 0.05715, 0]])
+    i = 1
+    while i < len(data):
+        line = data[i]
+        name = line[0]
+        mass = float(line[1]) * multiplier
+        com = np.array([float(x) for x in line[2:5]])
+        com_offset = np.array([float(x) for x in line[5:8]])
+        quat = np.array([float(x) for x in line[8:12]])
+        num_extra_masses = int(line[12])
+        extra_masses = []
+        for _ in range(num_extra_masses):
+            i += 1
+            extra_masses.append([float(x) for x in data[i][1:5]])
+        np_motors = np.array(extra_masses)
 
-print(adjust(mass, com, com_offset, quat, motors))
+        print(f"{name}:")
+        ans = adjust(mass, com, com_offset, quat, np_motors)
+        print(ans[0])
+        print(ans[1])
+        print(ans[2])
+        print()
+        i += 1
+
+def main():
+    multiplier = 1.0
+    calculate_com(multiplier)
+
+if __name__ == "__main__":
+    main()
+
 
 
 
